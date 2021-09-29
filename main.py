1d5f95202fd5a75896e478e78e368a12abd55bc8
rafsan ☀️ <18101009@uap-bd.edu>
2021-09-29 23:57:09 +0600

    week 3.1
    18101009
    added pause, resume method
    fixed pipes rgb errors
    added pause soundtrack
    added random pipes height
    
    18101007
    added pipes
    import random

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import pygame, random
 
+def pause():
+    paused = True
+    pygame.mixer.music.pause() #music pause
+
+    while paused:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_c:
+                    paused = False #if i press c, it will reume
+                    pygame.mixer.music.unpause() #music unpause
+
+                if event.key == pygame.K_q:
+                    pygame.quit()
+                    quit() #if i press q, then it will quit
+
 def base_movement(window, base_img, var_x): #animated base
     window.blit(base_img, (var_x, 640 - 112))  # display base image...   var_x is move from right to left position...
     # 640 =total screen height - base image height 112
@@ -9,7 +28,7 @@ def base_movement(window, base_img, var_x): #animated base
 def bird_movement(window, bird_img, bird_rect):
     window.blit(bird_img,bird_rect) #display bird image
 
-def pipe_movement(window,pipes, pipe_img):
+def pipe_movement(window, pipes, pipe_img):
 
     for pipe in pipes:
         pipe.centerx -= 5
@@ -17,6 +36,19 @@ def pipe_movement(window,pipes, pipe_img):
     for pipe in pipes:
         window.blit(pipe_img, pipe)
 
+def collision(pipes, bird_rect): #collision function
+    for pipe in pipes:
+        if pipe.colliderect(bird_rect):
+            print("Collided")
+
+    #upper & lower bound
+    if bird_rect.top <= -10:
+        print("Exceeded Upper Limit")
+
+    if bird_rect.bottom >= 640-112:
+        print("Exceeded Lower Limit")
+
+#--------------------------------------------------------------------------------------------------------#
 
 def game_build(): #function create for game build
 
@@ -26,7 +58,7 @@ def game_build(): #function create for game build
     #background music
     pygame.mixer.init() #initialize music
     pygame.mixer.music.load("gallery\\music\\rafsan.mp3") #music selection
-    pygame.mixer.music.set_volume(0.5) # range will be 0 to 1.. depends on volume
+    pygame.mixer.music.set_volume(0.25) # range will be 0 to 1.. depends on volume
     pygame.mixer.music.play(3) # 3 times continious play... if i put nothing... then it will play 1 time...
 
     bkg_img = pygame.image.load("gallery\\images\\bg.png") #background load
@@ -35,9 +67,9 @@ def game_build(): #function create for game build
 
     # bird
     bird_img = pygame.image.load("gallery\\images\\bird.png") #bird image load
-    bird_rect = bird_img.get_rect(center=(336/2,640/2)) #get rectangle function, 336/2. 640/2 will place the bird in center
+    bird_rect = bird_img.get_rect(center=(50,640/2)) #get rectangle function, 336/2. 640/2 will place the bird in center
     #center means... image will displayed from center
-    g_force = 0.3
+    g_force = 1
     bird_new_pos = 0
 
     #pipes
@@ -61,17 +93,23 @@ def game_build(): #function create for game build
                 running = False #cross button click
 
             if event.type == pygame.KEYDOWN:
-                 if event.key == pygame.K_DOWN:
-                     bird_new_pos = 0
-                     bird_new_pos -= 8
+                if event.key == pygame.K_UP:
+                     bird_new_pos -= 25
 
-            if event.type ==    TIMER:
-                random_pipe_height = [ 200,250,300,350,400]
-                pipes = pipe_img.get_rect(midtop = (240,random.choice(random_pipe_height)))
-                list_of_pipe.append(pipes)
+                if event.key == pygame.K_p:
+                    pause() #call of pause function
+
+            if event.type == TIMER:
+                random_pipe_height = [300, 350, 400, 450, 500, 550, 600] #pipe height
+                pipes = pipe_img.get_rect(midtop = (290,random.choice(random_pipe_height)))
+                list_of_pipe.append(pipes) #show the list of pipes
+
 
         #game logic
         window.blit(bkg_img,(0,0)) #display background... imagine graph pt(0,0) is center
+
+        #collition
+        collision(list_of_pipe, bird_rect)
 
         # pipe movement
         pipe_movement(window, list_of_pipe, pipe_img)
@@ -88,13 +126,13 @@ def game_build(): #function create for game build
         bird_rect.centery = bird_new_pos
         bird_movement(window, bird_img, bird_rect) #call the bird movement function
 
-
-        clock.tick(60) #frame rate... if i take low then it will lag, if i take high value, then it will run faster ....60fps
-
         # updating screen
+        clock.tick(60) #frame rate... if i take low then it will lag, if i take high value, then it will run faster ....60fps
         pygame.display.update()
 
     pygame.quit() #uninitialize pygame
+
+#--------------------------------------------------------------------------------------------------------#
 
 if __name__=="__main__":
     game_build() #call of game build func
